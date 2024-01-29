aa570d561d88056d68b5b987271a66f915cee756
Igor.U <i.usenko@dex-it.ru>
2024-01-29 17:37:27 +0200

    Portfolio v 0.1

--- personal-card/src/app/pages/home/home.component.ts
+++ personal-card/src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FooterComponent} from "../../layouts/footer/footer.component";
 import {HeaderComponent} from "../../layouts/header/header.component";
-import {RouterOutlet} from "@angular/router";
+import {ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {animate, style, transition, trigger} from "@angular/animations";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -10,19 +11,37 @@ import {animate, style, transition, trigger} from "@angular/animations";
   imports: [
     FooterComponent,
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    RouterLink,
+    NgForOf,
+    RouterLinkActive
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  // animations: [
-  //   trigger('changeBackgroundColor', [
-  //     transition(':enter', [
-  //       style({ backgroundColor: '#000000' }),
-  //       animate('3s', style({ backgroundColor: '#cbcbcb' }))
-  //     ])
-  //   ])
-  // ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  navItems: Array<string> = [
+    'ABOUT', 'EXPERIENCE', 'PROJECTS'
+  ];
+  activeRoute: string | null = '';
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.route.fragment.subscribe(fragment => {
+      this.activeRoute = fragment;
+    });
+  }
+
+
+  glowingCursor(event: any): void{
+    let x = event.clientX || event.touches[0].clientX
+    let y = event.clientY || event.touches[0].clientY
+
+    document.documentElement.style.setProperty('--cursorX', x + 'px')
+    document.documentElement.style.setProperty('--cursorY', y + 'px')
+  }
 
 }
